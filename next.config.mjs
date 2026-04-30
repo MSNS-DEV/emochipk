83d4250f066281020d2388c8d4e654b8d23b8ba1
h410m-h <hassaanazam7@gmail.com>
2026-04-30 22:42:57 +0500

    testDNS102

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,23 @@
 const nextConfig = {
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vqz56f5g7q.ufs.sh.global.cloud',
+      },
+    ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.executivemochi.pk/:path*',
+        permanent: true,
+        has: [{ type: 'host', value: 'executivemochi.pk' }],
+      },
+    ]
   },
 }
 
